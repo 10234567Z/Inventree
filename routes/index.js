8940049f066ad4afe35f40ac3ad8cb052a278394
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-01-23 07:26:54 +0530

    Complete category list request

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ const item_controller = require("../controllers/itemController")
 /* GET home page. */
 router.get('/', item_controller.index);
 
+router.get('/categories', category_controller.categories_list)
+
 module.exports = router;
