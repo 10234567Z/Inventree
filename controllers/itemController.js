e211de725171e1f1f1ec77ce763ba3d700331968
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-01-24 01:09:52 +0530

    Complete create item controller and route

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -38,3 +38,69 @@ exports.item_detail = asyncHandler(async (req , res , next ) => {
         item: item
     })
 })
+
+exports.item_create_get = asyncHandler(async (req , res , next) => {
+    const allCategories = await Category.find({} , "name").exec()
+    res.render("item_form", {
+        title: "Create Item",
+        allCategories: allCategories,
+      });
+})
+
+exports.item_create_post = [
+    body("name")
+        .trim()
+        .isLength({min: 3})
+        .escape()
+        .withMessage("Name too small")
+        .isLength({max:100})
+        .withMessage("Too many characters in name"),
+    body("description")
+        .trim()
+        .isLength({min: 10})
+        .escape()
+        .withMessage("Description too small")
+        .isLength({max:500})
+        .withMessage("Description word limit reached"),
+    body("price")
+        .trim()
+        .isInt({min: 1})
+        .escape()
+        .withMessage("Price too small"),
+    body("in_stock")
+        .trim()
+        .isInt({min: 1})
+        .escape()
+        .withMessage("Stock can't be in negative"),
+    asyncHandler(async (req , res , enxt) => {
+        const error = validationResult(req)
+
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            in_stock: req.body.in_stock
+        })
+
+        const allCategories = await Category.find({} , "name").exec()
+
+        if (!error.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render("item_form", {
+              title: "Create Item",
+              item: item,
+              allCategories: allCategories,
+              errors: error.array(),
+            });
+            return;
+          } else {
+            // Data from form is valid.
+      
+            // Save author.
+            await item.save();
+            // Redirect to new author record.
+            res.redirect(item.url);
+          }
+        })
+]
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/items' ,item_controller.item_list)
 router.get('/categories/:id' , category_controller.categories_detail)
 router.get('/items/:id', item_controller.item_detail)
 
+router.get('/item/create' , item_controller.item_create_get)
+router.post('/item/create' , item_controller.item_create_post)
+
+
 module.exports = router;
