3dad70dc6c446298ab92b66604861f573651887b
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-01-24 02:34:22 +0530

    Complete update categ. router and controllers

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -78,40 +78,99 @@ exports.category_create_post = [
     })
 ]
 
-exports.category_delete_get = asyncHandler(async (req , res , next) => {
-    const [category , category_items] = await Promise.all([
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+    const [category, category_items] = await Promise.all([
         Category.findById(req.params.id).exec(),
-        Item.find({category: req.params.id} , "name").exec()
+        Item.find({ category: req.params.id }, "name").exec()
     ])
 
 
-    if(category === null){
+    if (category === null) {
         res.redirect("/categories")
     }
 
-    res.render("category_delete" , {
+    res.render("category_delete", {
         title: "Delete Category",
         category: category,
         category_items: category_items
     })
 })
 
-exports.category_delete_post = asyncHandler(async (req , res , next)=> {
-    const [category , category_items] = await Promise.all([
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+    const [category, category_items] = await Promise.all([
         Category.findById(req.params.id).exec(),
-        Item.find({category: req.params.id} , "name").exec()
+        Item.find({ category: req.params.id }, "name").exec()
     ])
 
-    if(category_items.length > 0){
-        res.render("category_delete" , {
+    if (category_items.length > 0) {
+        res.render("category_delete", {
             title: "Delete Category",
             category: category,
             category_items: category_items
         })
         return
     }
-    else{
+    else {
         await Category.findByIdAndDelete(req.body.id)
         res.redirect("/categories")
     }
 })
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+    const category = await Category.findById(req.params.id).exec()
+
+    if (category === null) {
+        // No results.
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("category_form", {
+        title: "Create Category",
+        category: category
+    });
+})
+
+exports.category_update_post = [
+    body("name")
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage("Too small name for genre")
+        .isLength({ max: 100 })
+        .withMessage("Too long name for genre"),
+    body("description")
+        .trim()
+        .isLength({ min: 10 })
+        .escape()
+        .withMessage("Description too small")
+        .isLength({ max: 500 })
+        .withMessage("Description word limit reached"),
+    asyncHandler(async (req, res, next) => {
+        const error = validationResult(req)
+
+        const category = new Category({
+            _id: req.params.id,
+            name: req.body.name,
+            description: req.body.description
+        })
+
+        if (!error.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render("category_form", {
+                title: "Create Category",
+                category: category,
+                errors: error.array(),
+            });
+            return;
+        } else {
+            // Data from form is valid.
+
+            // Save author.
+            await Category.findByIdAndUpdate(req.params.id , category)
+            // Redirect to new author record.
+            res.redirect(category.url);
+        }
+    })
+]
--- routes/index.js
+++ routes/index.js
@@ -23,5 +23,10 @@ router.post('/items/:id/delete' , item_controller.item_delete_post)
 router.get('/categories/:id/delete' , category_controller.category_delete_get)
 router.post('/categories/:id/delete' , category_controller.category_delete_post)
 
+router.get('/categories/:id/update' , category_controller.category_update_get)
+router.post('/categories/:id/update' , category_controller.category_update_post)
+
+// router.get('/items/:id/update' , item_controller.item_update_get)
+// router.post('/items/:id/update' , item_controller.item_update_post)
 
 module.exports = router;
